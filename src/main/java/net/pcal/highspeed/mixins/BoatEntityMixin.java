58810ee70fea84d8f7f16f3dfbbe7b40721a40e5
Patrick Calahan <github@pcal.net>
2023-12-25 14:02:59 -0800

    checkpoint

--- src/main/java/net/pcal/highspeed/mixins/BoatEntityMixin.java
+++ src/main/java/net/pcal/highspeed/mixins/BoatEntityMixin.java
@@ -12,7 +12,7 @@ public class BoatEntityMixin {
 
     @SuppressWarnings("InvalidInjectorMethodSignature")
     // seems to be a plugin bug here, incorrectly complains about var/return types
-    @ModifyVariable(method = "checkLocation", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/vehicle/BoatEntity;getNearbySlipperiness()F"))
+    @ModifyVariable(method = "getStatus", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/entity/vehicle/Boat;getGroundFriction()F"))
     protected float limitIceSlipperiness(float original) {
         return HighspeedService.getInstance().isIceBoatsEnabled() ? original : Mth.clamp(original, 0f, 0.45f);
     }
